6b494d0acded2c0cc0866edbef18a5799935a626
Aja Strommen <9466667+astrommen@users.noreply.github.com>
2020-04-27 21:35:13 -0400

    got the scrape to push to mongo db using mongoose

--- server.js
+++ server.js
@@ -1,45 +1,80 @@
+// Dependencies
+var express = require("express");
+var exphbs = require("express-handlebars");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+
+// Scraping Tools
 var cheerio = require("cheerio");
 var axios = require("axios");
 
-// First, tell the console what server3.js is doing
-console.log("\n******************************************\n" +
-            "Look at the image of every award winner in \n" +
-            "one of the pages of `awwwards.com`. Then,\n" +
-            "grab the image's source URL." +
-            "\n******************************************\n");
+// Initialize models path
+var db = require("./models");
 
-// Make request via axios to grab the HTML from `awwards's` clean website section
-axios.get("http://www.sci-news.com/news/paleontology").then(function(response) {
+// Set Port
+var PORT = process.env.PORT || 3000;
 
-  // Load the HTML into cheerio
-  var $ = cheerio.load(response.data);
+// Initializa express
+var app = express();
 
-  // Make an empty array for saving our scraped info
-  var results = [];
+//Configure middleware
+// Set Handlebars as the default templating engine.
+// app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+// app.set("view engine", "handlebars");
+// Use morgan logger for requests
+app.use(logger("dev"));
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-  // With cheerio, look at each award-winning site, enclosed in "figure" tags with the class name "site"
-  $("div.bottom-archive").each(function(i, element) {
+// Make public static folder
+app.use(express.static("public"));
 
-    /* Cheerio's find method will "find" the first matching child element in a parent.
-     *    We start at the current element, then "find" its first child a-tag.
-     *    Then, we "find" the lone child img-tag in that a-tag.
-     *    Then, .attr grabs the imgs srcset value.
-     *    The srcset value is used instead of src in this case because of how they're displaying the images
-     *    Visit the website and inspect the DOM if there's any confusion
-    */
-    var title = $(element).find("h2").text();
-    var link = $(element).find("h2").children().attr("href");
-    var imgLink = $(element).find("img").attr("src").split(",")[0].split(" ")[0];
+// Connect to Mongo DB
+mongoose.connect("mongodb://localhost/paleontologyClippings", { useNewUrlParser: true });
 
-    // Push the image's URL (saved to the imgLink var) into the results array
-    results.push({ 
-      title: title,
-      link: link,
-      imglink: imgLink 
+// Routes
+
+// Get route for scraping sci-news website
+app.get("/scrape", function(req, res){
+  // Make request via axios to grab the HTML from `awwards's` clean website section
+  axios.get("http://www.sci-news.com/news/paleontology").then(function(response) {
+
+    // Load the HTML into cheerio
+    var $ = cheerio.load(response.data);
+
+    // Make an empty array for saving our scraped info
+    // With cheerio, look at each paleontology article, enclosed in "h2" tag
+    $("div.bottom-archive").each(function(i, element) {
+      var results = {};
+
+      results.title = $(element).find("h2").text();
+      results.link = $(element).find("h2").children().attr("href");
+      results.img = $(element).find("img").attr("src").split(",")[0].split(" ")[0];
+ 
+      // Create a new Artice using result object
+      db.Article.create(results).then(function(dbArticle) {
+        console.log(dbArticle);
+      }).catch(function(err) {
+        console.log(err);
+      });
     });
 
+    // console.log(results);
+    res.send("Scrape Complete");
   });
+});
 
-  // After looping through each element found, log the results to the console
-  console.log(results);
+app.get("/articles", function(req, res) {
+  // Grabb all documents in Articles collection
+  db.Article.find({}).then(function(dbArticle) {
+    res.json(dbArticle);
+  }).catch(function(err){
+    res.json(err);
+  });
+});
+
+// Start the server
+app.listen(PORT, function() {
+  console.log("App running on port " + PORT + "!");
 });
